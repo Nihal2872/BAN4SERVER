64e31971df426fc1694360816f024a111ab74beb
Nihal2872 <115912390+Nihal2872@users.noreply.github.com>
2022-12-01 02:32:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-os.system("git clone https://Nihal2872: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@github.com/Nihal2872/USERBOT okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m USERserver1 &")
+os.system("git clone https://Nihal2872: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@github.com/Nihal2872/USERBOT okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m main.py &")
